830e28316067f054eb196724ea900b9f14dde89a
Michael Kong <michaelkong@Michaels-MBP.gateway>
2018-07-09 01:07:27 +1000

    remove unused add and mul safemath functions

--- contracts/solidity/MultiSigWallet.sol
+++ contracts/solidity/MultiSigWallet.sol
@@ -13,19 +13,9 @@ pragma solidity 0.4.23;
 
 library SafeMath {
 
-    function add(uint a, uint b) internal pure returns (uint c) {
-        c = a + b;
-        require(c >= a);
-    }
-
     function sub(uint a, uint b) internal pure returns (uint c) {
         require(b <= a);
         c = a - b;
-    }
-
-    function mul(uint a, uint b) internal pure returns (uint c) {
-        c = a * b;
-        require(a == 0 || c / a == b);
     }
 
 }
